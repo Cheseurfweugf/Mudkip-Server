8ce56cf57fdfc247b6ee17905dcf8eb2bd9f1e2f
Cheseurfweugf <chesandmary@gmail.com>
2016-01-27 12:58:51 -0300

    Update getserver.js

--- bot/getserver.js
+++ bot/getserver.js
@@ -17,7 +17,7 @@ if (process.argv[2]) {
 
 	var received = false;
 	var requestOptions = {
-		hostname: 'http://leaf-megaforever.rhcloud.com',
+		hostname: 'leaf-chesnaughtmega.c9users.io',
 		port: 80,
 		path: '/crossdomain.php?host=' + serverUrl + '&path=',
 		method: 'GET'
